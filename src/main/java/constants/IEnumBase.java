97553d984d5bb66a9615da6ad0820ce5f37465c0
inret-river <a1ph4dd@gmail.com>
2022-08-06 00:46:48 +0900

    initail commit

---
+++ src/main/java/constants/IEnumBase.java
@@ -0,0 +1,28 @@
+package main.java.constants;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public interface IEnumBase {
+
+	@SuppressWarnings("unchecked")
+	public static <E extends Enum<E> & IEnumBase> Optional<E> getConstByKey(String key, E... obj) {
+
+		if (Objects.isNull(obj)) {
+			// 実装不良
+			throw new AssertionError();
+		}
+		
+		E[] elements = obj.length == 0 ? (E[]) obj.getClass().getEnumConstants() : obj;
+
+		for (E elem : elements) {
+			if (elem.getKey().equals(key)) {
+				return Optional.of(elem);
+			}
+		}
+		return Optional.empty();
+	}
+
+	String getKey();
+
+}
